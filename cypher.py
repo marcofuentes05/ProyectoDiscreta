c79b89f3e8ad380ab4eda80bf9413a6c4fc2f9dc
Marco Fuentes <marcofuentes1607@gmail.com>
2019-11-06 08:23:05 -0600

    Commit inicial

---
+++ cypher.py
@@ -0,0 +1,14 @@
+"""-----------------------------------------
+cypher.py
+Programa que encripta/desencripta un mensaje usando el algoritmo RSA
+para el proye to final de Matemática Discreta 1
+
+Autores: 
+	Marco Fuentes
+	Andy Castillo
+	Gian Luca Rivera
+Universidad del Valle de Guatemala
+2do semestre de 2019
+"""
+
+import numpy as np
